9cffacd64c90a0117d8a2b03d5f441d11d826a31
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-17 11:41:25 -0700

    Update Force_Frenzy.js

--- Hacks/Blooket/FishingFrenzy/Force_Frenzy.js
+++ Hacks/Blooket/FishingFrenzy/Force_Frenzy.js
@@ -1,1 +1,15 @@
+const forceFrenzy = () => {
+    const { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+        {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
+    })())[1].children[0]._owner;
 
+    stateNode.props.liveGameController.setVal({
+        path: `c/${stateNode.props.client.name}`,
+        val: {
+            b: stateNode.props.client.blook,
+            w: stateNode.state.weight,
+            f: "Frenzy",
+            s: true
+        }
+    })
+}
