ca5d8c1c4051ff02b47d72e9d36de3b355d407eb
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-17 13:02:18 -0700

    Update AutoAnswer.js

--- Hacks/Blooket/Global/AutoAnswer.js
+++ Hacks/Blooket/Global/AutoAnswer.js
@@ -1,11 +1,11 @@
 const autoAnswer = () => {
     setInterval(() => {
-        const { stateNode : {questions, props, state} } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+        const { stateNode : {questions, state} } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
             {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
         })())[1].children[0]._owner;
 
-        if (state.stage == "question") if (question.qType != "typing") {
-            document.querySelectorAll(`[class*=answerContainer]`)[questions[state.question.number - 1].answers.map((x, i) => x == questions[state.question.number - 1].correctAnswers.includes(x) ? i : null).filter(x => x !== null)[0]]?.click?.();
+        if (state.stage == "question") if (state.question.qType != "typing") {
+            [...document.querySelectorAll(`[class*=answerContainer]`)][questions[state.question.number - 1].answers.map((x, i) => questions[state.question.number - 1].correctAnswers.includes(x) ? i : null).filter(x => x !== null)[0]]?.click?.();
         } else {
             Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(questions[state.question.number - 1].answers[0]);
         }
