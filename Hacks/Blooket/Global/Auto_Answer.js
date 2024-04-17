0299a5f3c911bb79affe966ffbf9d4a6b112d696
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-17 13:14:10 -0700

    Update and rename AutoAnswer.js to Auto_Answer.js

--- Hacks/Blooket/Global/Auto_Answer.js
+++ Hacks/Blooket/Global/Auto_Answer.js
@@ -5,11 +5,17 @@ const autoAnswer = () => {
         })())[1].children[0]._owner;
 
         if (state.stage == "question") if (state.question.qType != "typing") {
-            [...document.querySelectorAll(`[class*=answerContainer]`)][questions[state.question.number - 1].answers.map((x, i) => questions[state.question.number - 1].correctAnswers.includes(x) ? i : null).filter(x => x !== null)[0]]?.click?.();
+            [...document.querySelectorAll(`[class*=answerContainer]`)].forEach(answer => {
+                if (answer.innerHTML.includes(questions[state.question.number - 1].correctAnswers[0])) {
+                    answer?.click();
+                }
+            });
         } else {
             Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(questions[state.question.number - 1].answers[0]);
         }
     }, 500);
+
+    console.log(stateNode);
 }
 
 autoAnswer();
