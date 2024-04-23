6293a8aa738625e05d76f42a1971562b4cea9c96
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-23 14:08:00 -0700

    Create Max_Level_Food.js

---
+++ Hacks/Blooket/Cafe/Max_Level_Food.js
@@ -0,0 +1,11 @@
+const maxLevelFood = () => {
+    const { stateNode : { state } } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+        {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
+    })())[1].children[0]._owner;
+
+    state.foods.forEach(food => {
+        food.level = 4;
+    });
+}
+
+maxLevelFood();
