fef33b5392d3116a65e1277ca7f80e71ef5a57c5
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-23 09:04:25 -0700

    Update Auto_Answer.js

--- Hacks/Blooket/Global/Auto_Answer.js
+++ Hacks/Blooket/Global/Auto_Answer.js
@@ -1,25 +1,25 @@
 const autoAnswer = () => {
     setInterval(() => {
-        const { stateNode : {questions, state, props} } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+        const { stateNode : {state, props} } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
             {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
         })())[1].children[0]._owner;
 
-        if (state.stage && state.stage == "question") if (state.question.qType != "typing") { 
+        if (state.stage && state.stage == "question") if (state.question.qType != "typing") {
             let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
 
             [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(state.question.correctAnswers[0]) ? i : null).filter(i => i !== null)[0]].click(); 
         } else {
-            Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(questions[state.question.number - 1].answers[0]);
+            Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(state.question.answers[0]);
         } else if (window.location.href.includes('battleroyale')) {
+            let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
+
             if (document.querySelector("[class*='typingAnswerWrapper']")) {
                 Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(props.client.question.answers[0]);
-            } else {
-                let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
-
+            } else if (answerContainers) {
                 [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(props.client.question.correctAnswers[0]) ? i : null).filter(i => i !== null)[0]].click();
             }
         }
-    }, 500);
+    }, 450);
 }
 
 autoAnswer();
