bca10bd3403117f5f7d6432d7410f632770123bb
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-24 09:26:24 -0700

    Update Max_Stock_Food.js

--- Hacks/Blooket/Cafe/Max_Stock_Food.js
+++ Hacks/Blooket/Cafe/Max_Stock_Food.js
@@ -1,9 +1,9 @@
 const maxStockFood = () => {
-    const { stateNode : { state, setState } } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+    const { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
         {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
     })())[1].children[0]._owner;
 
-    setState({ foods: state.foods.map(e => ({ ...e, stock: 99, level: e.level })) })
+    stateNode.setState({ foods: stateNode.state.foods.map(e => e.level == 1 ? ({ ...e, stock: 99, level: e.level }) : ({ ...e, stock: e.stock, level: e.level })) })
 }
 
 maxStockFood();
