b20e09ca48c56ea4c3feb0696fa5177db95fe2ca
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-04-24 09:27:08 -0700

    Update Unlock_All_Food.js

--- Hacks/Blooket/Cafe/Unlock_All_Food.js
+++ Hacks/Blooket/Cafe/Unlock_All_Food.js
@@ -1,9 +1,9 @@
 const unlockAllFoods = () => {
-    const { stateNode : { state, setState } } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+    const { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
         {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
     })())[1].children[0]._owner;
 
-    setState({ foods: state.foods.map(e => ({ ...e, stock: 0, level: 1 })) })
+    stateNode.setState({ foods: stateNode.state.foods.map(e => ({ ...e, stock: e.stock, level: 1 })) })
 }
 
 unlockAllFoods();
