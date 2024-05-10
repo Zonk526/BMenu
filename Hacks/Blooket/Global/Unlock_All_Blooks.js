2967c8e252766810b2f912c8ebb10f48bc429d69
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-05-10 11:00:18 -0700

    Update Unlock_All_Blooks.js

--- Hacks/Blooket/Global/Unlock_All_Blooks.js
+++ Hacks/Blooket/Global/Unlock_All_Blooks.js
@@ -1,16 +1,9 @@
-var blooksArraySelect = Object.values(document.querySelector(`div[class^='styles__blooksHolder']`))[1].children[0];
-let listOfBlookNames = [];
-
-// unlock all blooks
-
-let { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')){
-    return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'))
-})())[1].children[0]._owner;
-
-for (let child of blooksArraySelect) {
-    let childsKey = child.key;
-
-    listOfBlookNames.push(childsKey);
+const unlockAllBlooks = () => {
+    const { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')){
+        return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'))
+    })())[1].children[0]._owner;
+    
+    stateNode.setState({unlocks: Object.values(document.querySelector(`[class*='blooksHolder']`))[1].children[0].map(x => x.key)});
 }
 
-stateNode.state.unlocks = listOfBlookNames;
+unlockAllBlooks();
