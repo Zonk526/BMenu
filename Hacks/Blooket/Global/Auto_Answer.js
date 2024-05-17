208987f38655e859ebe0dc33a4ffd4b27373231f
Zonk526 <124474260+Zonk526@users.noreply.github.com>
2024-05-17 12:02:12 -0700

    Update Auto_Answer.js

--- Hacks/Blooket/Global/Auto_Answer.js
+++ Hacks/Blooket/Global/Auto_Answer.js
@@ -1,22 +1,25 @@
 const autoAnswer = () => {
     setInterval(() => {
-        const { stateNode : {state, props} } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
+        const { stateNode } = Object.values((function react(potentialDiv = document.querySelector('body>div')) 
             {return Object.values(potentialDiv)[1]?.children?.[0]?._owner.stateNode ? potentialDiv : react(potentialDiv.querySelector(':scope>div'));
         })())[1].children[0]._owner;
 
-        if (state.stage && state.stage == "question") if (state.question.qType != "typing") {
-            let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
+        let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
 
-            [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(state.question.correctAnswers[0]) ? i : null).filter(i => i !== null)[0]].click(); 
+        if (stateNode.state.stage && stateNode.state.stage == "question") if (stateNode.state.question.qType != "typing") {
+            stateNode.onAnswer ? stateNode.onAnswer('shitty website lol') : 
+            [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(stateNode.state.question.correctAnswers[0]) ? i :
+            null)[0]]?.click()
+
         } else {
-            Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(state.question.answers[0]);
-        } else if (window.location.href.includes('battleroyale')) {
-            let answerContainers = document.querySelectorAll(`[class*='answerContainer']`);
+            Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(stateNode.state.question.answers[0]);
 
+        } else if (window.location.href.includes('battleroyale')) {
             if (document.querySelector("[class*='typingAnswerWrapper']")) {
-                Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(props.client.question.answers[0]);
+                Object.values(document.querySelector("[class*='typingAnswerWrapper']"))[1].children._owner.stateNode.sendAnswer(stateNode.props.client.question.answers[0]);
+
             } else if (answerContainers) {
-                [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(props.client.question.correctAnswers[0]) ? i : null).filter(i => i !== null)[0]].click();
+                [...answerContainers][Array.from(answerContainers).map((x, i) => x.innerHTML.includes(stateNode.props.client.question.correctAnswers[0]) ? i : null).filter(i => i !== null)[0]].click();
             }
         }
     }, 450);
